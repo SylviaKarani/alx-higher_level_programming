97e7073da8ef733e15fb3e746205920e58027aae
Sylvia Karani <107038992+SylviaKarani@users.noreply.github.com>
2023-01-09 14:26:50 +0300

    committed changes

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    userin = argv[1:]
+    size = len(userin)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if size != 1 else "argument",
+                 "." if size == 0 else ":"))
+    for idx, arg in enumerate(userin):
+        print("{:d}: {:s}".format(idx + 1, arg))
